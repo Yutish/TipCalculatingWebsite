842606b3489390a0da0c1b86ca31dbc064b72d19
Yutish <yutishfarma21offi@gmail.com>
2019-09-05 17:35:55 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+//custom function
+
+    
+function calculateTip(){
+    //store the data of input
+    var billAmount = document.getElementById("billAmount").value;
+    var serviceQuality = document.getElementById("serviceQuality").value;
+    var numPeople =document.getElementById("totalPeople").value;
+    
+    if(serviceQuality == 0 || billAmount === ""){
+        window.alert("Please fill the values");
+        return;
+    }
+    
+    if(numPeople==="" || numPeople<=1){
+        numPeople=1;
+        document.getElementById("each").style.display="none";
+    }
+    else{
+        document.getElementById("each").style.display="block";   
+    }
+    
+    var total=(billAmount*serviceQuality)/numPeople;
+    
+    total=Math.round(total*100)/100;
+    total.toFixed(2);
+    
+    document.getElementById("totalTip").style.display="block";
+    
+    document.getElementById("tip").innerHTML=total;
+}
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display="none";
+
+document.getElementById("calculate").onclick = function(){ calculateTip();};
